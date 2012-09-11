acf36c40967eea50720fbba4432f141fcf480276
Julien Sarazin <julien.sarazin@me.com>
2012-09-11 12:53:03 +0200

    Initial Commit

---
+++ iReader-RSS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iReader-RSS
+//
+//  Created by Julien Sarazin on 11/09/12.
+//  Copyright (c) 2012 Julien Sarazin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iReader-RSS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iReader-RSS
+//
+//  Created by Julien Sarazin on 11/09/12.
+//  Copyright (c) 2012 Julien Sarazin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
