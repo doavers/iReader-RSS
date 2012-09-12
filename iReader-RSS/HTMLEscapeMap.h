ef9410ffa03c7f8c14619533662546f2e6de186e
Julien Sarazin <julien.sarazin@me.com>
2012-09-13 01:35:22 +0200

    Stop coding start sleeping
    Stop being an ass and go to bed u comitted too much shit.

---
+++ iReader-RSS/HTMLEscapeMap.h
@@ -0,0 +1,13 @@
+//
+//  HTMLEscapeMap.h
+//  iReader-RSS
+//
+//  Created by Julien Sarazin on 12/09/12.
+//  Copyright (c) 2012 Julien Sarazin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTMLEscapeMap : NSObject
+
+@end
---
+++ iReader-RSS/HTMLEscapeMap.m
@@ -0,0 +1,13 @@
+//
+//  HTMLEscapeMap.m
+//  iReader-RSS
+//
+//  Created by Julien Sarazin on 12/09/12.
+//  Copyright (c) 2012 Julien Sarazin. All rights reserved.
+//
+
+#import "HTMLEscapeMap.h"
+
+@implementation HTMLEscapeMap
+
+@end
